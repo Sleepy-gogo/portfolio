343ff72e323997a161e2357f6f5d6661d8daae0d
Axel Castaño <axelcastano@proton.me>
2025-10-18 02:44:36 -0300

    Add initial showcase section

--- src/lib/project-data.ts
+++ src/lib/project-data.ts
@@ -1,2 +1,15 @@
+export interface Project {
+  image: string;
+  title: string;
+  description: string;
+  tags: string[];
+};
 
-export type TechId = "next" | "astro" | "tailwind" | "typescript" | "react" | "svelte" | "convex" | "mongo" | "betterauth" | "clerk" | "sqlite" | "drizzle";
+export const projects: Project[] = [
+  {
+    title: "Coonspace",
+    description: "Coonspace is a markdown sharing platform where users can create, edit, and share their markdown documents freely.",
+    tags: ["NextJS", "TailwindCSS", "DrizzleORM", "SQLite", "Typescript", "Clerk"],
+    image: "/projects/coonspace.webp"
+  }
+];
